90fe3fd7607da34c9e5f61f661e79ca4404964bc
dongkaijie <504814319@qq.com>
2020-02-21 10:46:58 +0800

    add thrift server client

---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+sys.path.append('./gen-py')
+
+from HelloWorld import HelloWorld
+from HelloWorld.ttypes import *
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+class HelloWorldHandler:
+    def ping(self):
+        return "pong"
+
+    def say(self, msg):
+        ret = "Received: " + msg
+        print ret
+        return ret
+
+handler = HelloWorldHandler()
+
+processor = HelloWorld.Processor(handler)
+
+transport = TSocket.TServerSocket("localhost", 9090)
+tfactory = TTransport.TBufferedTransportFactory()
+pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+print "Starting thrift server in python..."
+server.serve()
+print "done!"
